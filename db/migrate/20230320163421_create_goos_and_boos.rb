c65ad2e1db21465d2613d803e814afb6279d366d
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-03-20 18:14:49 +0100

    Test associations
    ```
    goo = Goo.create name: 'First Goo'
    goo.create_boo name: '1 Boo'
    goo.boo.destroy
    goo.create_boo name: '2 Boo'
    goo.destroy
    ```
    
    without `touch: true`
    
    ```
    Goo.history.first.boo
    => nil
    
    Boo.history.first.goo
    => #<Goo, id: 1, name: "First Goo">
    
    Boo.history.last.goo
    => nil
    
    ```
    
    with `touch: true` on both directions
    
    ```
    same results
    ```

--- db/migrate/20230320163421_create_goos_and_boos.rb
+++ db/migrate/20230320163421_create_goos_and_boos.rb
@@ -1,10 +1,12 @@
 class CreateGoosAndBoos < ActiveRecord::Migration[7.0]
   def change
     create_table :goos, temporal: true do |t|
+      t.string :name, null: false
       t.timestamps
     end
 
     create_table :boos, temporal: true do |t|
+      t.string :name, null: false
       t.references :goo
       t.timestamps
     end
