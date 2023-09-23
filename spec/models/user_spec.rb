0dba3222e57e6a2f925bfb5661d50f861437bb10
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-09-23 12:05:51 +0200

    Use main branch

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  it 'works' do
-    expect {
-      described_class
-    }.not_to raise_error
+  describe 'factory' do
+    it 'works' do
+      expect {
+        build(:user)
+      }.not_to raise_error
+    end
   end
 end
