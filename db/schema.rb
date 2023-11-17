3c839031021362139ec9819bb51fdc861b337dcb
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-11-17 20:14:18 +0100

    Reproducible test case
    ```
    bundle exec rspec spec/model/user_spec.rb 
    ```
    
    The problem happens when using traits

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_05_152838) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_17_190606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
