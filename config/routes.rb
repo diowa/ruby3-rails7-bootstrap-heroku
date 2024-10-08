e0556bfe88515301310e2fa07b0025f045ea3f57
Geremia Taglialatela <tagliala@users.noreply.github.com>
2024-10-08 19:07:30 +0200

    Add default health check endpoint (#986)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
   root 'pages#home'
   get :hello_world, to: 'pages#hello_world'
 end
