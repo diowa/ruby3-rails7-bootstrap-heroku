d2b225aa3543973298141313246725af68358423
Geremia Taglialatela <tagliala.dev@gmail.com>
2025-11-13 10:13:14 +0100

    Improvement by Claude

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,29 +1,22 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig } = require('shakapacker')
+const { generateWebpackConfig, merge } = require('shakapacker')
 
-// Get the base webpack config from shakapacker
 const webpackConfig = generateWebpackConfig()
 
-// Find and modify the sass-loader rule to silence deprecation warnings
-if (webpackConfig.module && webpackConfig.module.rules) {
-  webpackConfig.module.rules.forEach(rule => {
-    // Find the rule that handles SCSS/SASS files
-    if (rule.test && rule.test.toString().includes('scss') && Array.isArray(rule.use)) {
-      rule.use.forEach(loader => {
-        // Find the sass-loader in the use array
-        if (loader && loader.loader && loader.loader.includes('sass-loader')) {
-          // Add silenceDeprecations to the sassOptions
-          loader.options = loader.options || {}
-          loader.options.sassOptions = loader.options.sassOptions || {}
-          loader.options.sassOptions.silenceDeprecations = [
-            'color-functions',
-            'global-builtin',
-            'import'
-          ]
+const customConfig = {
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: 'sass-loader',
+        options: {
+          sassOptions: {
+            silenceDeprecations: ['color-functions', 'global-builtin', 'import']
+          }
         }
-      })
-    }
-  })
+      }]
+    }]
+  }
 }
 
-module.exports = webpackConfig
+module.exports = merge(webpackConfig, customConfig)
